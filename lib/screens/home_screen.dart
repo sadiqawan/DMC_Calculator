a73abe11568bf3e24829c7717e90fe2e5d7332b7
sadiqawan <awansadiq09@gmail.com>
2024-01-28 01:42:46 +0500

    validation are add

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -10,10 +8,19 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+
+
+
 class _HomeScreenState extends State<HomeScreen> {
+
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  late int eng, urdu, math, sci, isl;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black26,
       appBar: AppBar(
         title: const Center(
           child: Text(
@@ -23,59 +30,145 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         backgroundColor: Colors.black,
       ),
-
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Form(child: ListView(
-          children: [
-           const Gap(20),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const  InputDecoration(
-                hintText:'Enter English marks',
-                labelText: 'English',
-                border: OutlineInputBorder(),
+        child: Form(
+          key: formkey,
+          child: ListView(
+            children: [
+              const Gap(20),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: 'Enter English marks',
+                  hintStyle: TextStyle(color: Colors.white),
+                  labelText: 'English',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                ),
+
+                validator: (text){
+                  if( text == null || text.isEmpty){
+                    return 'Please enter marks';
+                  }
+                  if( text.length > 3){
+                    return ' inValid Marks';
+                  }
+                  if ( int.parse(text) >= 100){
+                    return 'Marks can not be grater than 100';
+                  }
+                  eng = int.parse(text);
+                  return null;
+                },
+
               ),
-            ),
-           const  Gap(20),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const  InputDecoration(
-                hintText:'Enter Urdu marks',
-                labelText: 'Urdu',
-                border: OutlineInputBorder(),
+              const Gap(20),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Urdu marks',
+                  hintStyle: TextStyle(color: Colors.white),
+                  labelText: 'Urdu',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (text){
+                  if( text == null || text.isEmpty){
+                    return 'Please enter marks';
+                  }
+                  if( text.length > 3){
+                    return ' inValid Marks';
+                  }
+                  if ( int.parse(text) >= 100){
+                    return 'Marks can not be grater than 100';
+                  }
+                  urdu = int.parse(text);
+                  return null;
+                },
               ),
-            ),
-            const  Gap(20),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const  InputDecoration(
-                hintText:'Enter Math marks',
-                labelText: 'Math',
-                border: OutlineInputBorder(),
+              const Gap(20),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Math marks',
+                  hintStyle: TextStyle(color: Colors.white),
+                  labelText: 'Math',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (text){
+                  if( text == null || text.isEmpty){
+                    return 'Please enter marks';
+                  }
+                  if( text.length > 3){
+                    return ' inValid Marks';
+                  }
+                  if ( int.parse(text) >= 100){
+                    return 'Marks can not be grater than 100';
+                  }
+                  math= int.parse(text);
+                  return null;
+                },
               ),
-            ),
-            const  Gap(20),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const  InputDecoration(
-                hintText:'Enter Science marks',
-                labelText: 'Science ',
-                border: OutlineInputBorder(),
+              const Gap(20),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Science marks',
+                  hintStyle: TextStyle(color: Colors.white),
+                  labelText: 'Science ',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (text){
+                  if( text == null || text.isEmpty){
+                    return 'Please enter marks';
+                  }
+                  if( text.length > 3){
+                    return ' inValid Marks';
+                  }
+                  if ( int.parse(text) >= 100){
+                    return 'Marks can not be grater than 100';
+                  }
+                  sci = int.parse(text);
+                  return null;
+                },
               ),
-            ),
-            const  Gap(20),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const  InputDecoration(
-                hintText:'Enter Islamiat marks',
-                labelText: 'Islamiat',
-                border: OutlineInputBorder(),
+              const Gap(20),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Islamiat marks',
+                  hintStyle: TextStyle(color: Colors.white),
+                  labelText: 'Islamiat',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (text){
+                  if( text == null || text.isEmpty){
+                    return 'Please enter marks';
+                  }
+                  if( text.length > 3){
+                    return ' inValid Marks';
+                  }
+                  if ( int.parse(text) >= 100){
+                    return 'Marks can not be grater than 100';
+                  }
+                  isl = int.parse(text);
+                  return null;
+                },
               ),
-            ),
-            const  Gap(20),
-          ],
-        )),
+              const Gap(20),
+              ElevatedButton(onPressed: () {}, child: const Text('Calculate')),
+              const Gap(20),
+              ElevatedButton(onPressed: () {
+                setState(() {
+
+                });
+              }, child: const Text('Clear'))
+            ],
+          ),
+        ),
       ),
     );
   }
