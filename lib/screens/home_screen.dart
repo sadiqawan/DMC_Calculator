dc026ff0333a42dd8cea41dc1a9b965bb7b40531
sadiqawan <awansadiq09@gmail.com>
2024-01-28 01:06:36 +0500

    TextFormFields are added to HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,70 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'DMC',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(child: ListView(
+          children: [
+           const Gap(20),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const  InputDecoration(
+                hintText:'Enter English marks',
+                labelText: 'English',
+                border: OutlineInputBorder(),
+              ),
+            ),
+           const  Gap(20),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const  InputDecoration(
+                hintText:'Enter Urdu marks',
+                labelText: 'Urdu',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const  Gap(20),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const  InputDecoration(
+                hintText:'Enter Math marks',
+                labelText: 'Math',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const  Gap(20),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const  InputDecoration(
+                hintText:'Enter Science marks',
+                labelText: 'Science ',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const  Gap(20),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const  InputDecoration(
+                hintText:'Enter Islamiat marks',
+                labelText: 'Islamiat',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const  Gap(20),
+          ],
+        )),
+      ),
+    );
   }
 }
